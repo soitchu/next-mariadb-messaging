5cc04379401c149fbd71e38aa559f37b51b83c8f
soitchu <soitchu@users.noreply.github.com>
2023-11-21 12:16:21 -0800

    feat: added markdown

--- Components/ChatList/Message.tsx
+++ Components/ChatList/Message.tsx
@@ -1,6 +1,8 @@
+import remarkGfm from "remark-gfm";
 import styles from "../../styles/Message.module.css";
 import { openContextMenu } from "../ContextMenu";
 import { dateToHuman } from "../helperClient";
+import Markdown from "react-markdown";
 
 function handleClick(event) {
   console.log(event.currentTarget.classList.contains(styles.right));
@@ -12,7 +14,9 @@ function handleClick(event) {
 export default function Message({ content, time, align, id }) {
   return (
     <div className={styles.message + " " + styles[align]} data-id={id} onContextMenu={handleClick}>
-      {content}
+      <Markdown remarkPlugins={[remarkGfm]}>
+        {content}
+      </Markdown>
       <div className={styles.time}>
         {dateToHuman(time)}
       </div>
